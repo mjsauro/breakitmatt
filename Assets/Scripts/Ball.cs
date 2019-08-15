3f27224ea66b2311d601384474ca0b28034adda4
mjsauro <mjsauro@gmail.com>
2019-08-14 23:07:57 -0500

    ball now launches

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -6,23 +6,39 @@ public class Ball : MonoBehaviour
 {
     // configuration parameters
     [SerializeField] private Paddle paddle1;
+    [SerializeField] private float xPush = 2f;
+    [SerializeField] private float yPush = 15f;
 
     // state
     Vector2 paddleToBallVector;
-
+    bool hasStarted = false;
     // Use this for initialization
 
     void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (hasStarted) return;
+        LockBallToPaddle();
+        LaunchBallOnMouseClick();
+    }
+
+    private void LaunchBallOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
         Vector2 paddlePosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePosition + paddleToBallVector;
-
     }
 }
