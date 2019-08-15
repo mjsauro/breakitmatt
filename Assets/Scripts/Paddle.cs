a7723fdc945b91f35d748a17261ae0ef0c29c539
mjsauro <mjsauro@gmail.com>
2019-08-14 22:42:19 -0500

    math.clamp keeping the paddle within the bounds of the screen

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,7 +1,11 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Paddle : MonoBehaviour {
 
+	// configuration parameters
+	[SerializeField] private float minX = 1f;
+	[SerializeField] private float maxX = 15f;
 	[SerializeField] private float screenWidthInUnits = 16f;
 	
 
@@ -10,7 +14,8 @@ public class Paddle : MonoBehaviour {
 
 		float mousePositionInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits; 
 
-		Vector2 paddlePosition = new Vector2(mousePositionInUnits, transform.position.y);
+		Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
+		paddlePosition.x = Mathf.Clamp(mousePositionInUnits, minX, maxX);
 		transform.position = paddlePosition;
 
 
