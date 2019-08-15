653cf420c0900988070aea781323d6db69dc8ff2
mjsauro <mjsauro@gmail.com>
2019-08-14 22:34:43 -0500

    moving the mouse around

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+	[SerializeField] private float screenWidthInUnits = 16f;
+	
+
+	// Update is called once per frame
+	private void Update () {
+
+		float mousePositionInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits; 
+
+		Vector2 paddlePosition = new Vector2(mousePositionInUnits, transform.position.y);
+		transform.position = paddlePosition;
+
+
+	}
+}
