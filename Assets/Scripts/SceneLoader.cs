19965fabc2fbe0792926ca2f1a7c45e5c0dc2d8c
mjsauro <mjsauro@gmail.com>
2019-08-14 21:57:31 -0500

    added game over

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -6,18 +6,19 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour {
 
 	public void LoadNextScene()
-    {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(currentSceneIndex + 1);
-    }
+	{
+		int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+		SceneManager.LoadScene(currentSceneIndex + 1);
+	}
 
-    public void LoadStartScene()
-    {
-        SceneManager.LoadScene(0);
-    }
+	public void LoadStartScene()
+	{
+		SceneManager.LoadScene(0);
+	}
 
-    public void QuitGame()
-    {
-        Application.Quit();
-    }
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
 }
