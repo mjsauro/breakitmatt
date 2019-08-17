8d93ed13f0bd478221819e4dfbc2b5d1d293d083
mjsauro <mjsauro@gmail.com>
2019-08-17 17:45:04 -0500

    added sounds, changed friction

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = System.Random;
 
 public class Ball : MonoBehaviour
 {
@@ -9,14 +11,21 @@ public class Ball : MonoBehaviour
     [SerializeField] private float xPush = 2f;
     [SerializeField] private float yPush = 15f;
 
+    [SerializeField] private AudioClip[] ballSounds;
+
     // state
     Vector2 paddleToBallVector;
     bool hasStarted = false;
+
+    //cached component references
+    AudioSource myAudioSource;
+
     // Use this for initialization
 
     void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -40,5 +49,14 @@ public class Ball : MonoBehaviour
     {
         Vector2 paddlePosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePosition + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasStarted)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+        }
     }
 }
