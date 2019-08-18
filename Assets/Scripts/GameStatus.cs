93e319cdb2ea09b5391b69c0a096f89cea6a0c35
mjsauro <mjsauro@gmail.com>
2019-08-17 21:40:28 -0500

    implemented score and points

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,25 +1,37 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
+using TMPro;
 
-public class GameStatus : MonoBehaviour {
+public class GameStatus : MonoBehaviour
+{
 
-	//parameters
-	[Range(0.1f, 10f)] [SerializeField] private float gameSpeed = 1f;
-	[SerializeField] private int pointsPerBlockDestroyed = 83;
-	
-	// state variables
-	[SerializeField] private int currentScore = 0;
+    //parameters
+    [Range(0.1f, 10f)] [SerializeField] private float gameSpeed = 1f;
+    [SerializeField] private int pointsPerBlockDestroyed = 83;
+    [SerializeField] private TextMeshProUGUI scoreText;
 
-	// Update is called once per frame
-	void Update ()
-	{
-		Time.timeScale = gameSpeed;
-	}
+    // state variables
+    [SerializeField] private int currentScore = 0;
 
-	public void AddToScore()
-	{
-		currentScore += pointsPerBlockDestroyed;
-	}
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+
+    private void Update()
+    {
+        Time.timeScale = gameSpeed;
+
+    }
+
+    public void AddToScore()
+    {
+        currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
+
+    }
 }
