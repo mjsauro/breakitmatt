f3c152957c19c937404b06fbcdea12c9c5609629
mjsauro <mjsauro@gmail.com>
2019-08-18 11:40:04 -0500

    added ball randomness

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -7,6 +7,7 @@ public class Ball : MonoBehaviour
     [SerializeField] private float xPush = 2f;
     [SerializeField] private float yPush = 15f;
     [SerializeField] private AudioClip[] ballSounds;
+    [SerializeField] private float randomFactor = 0.2f;
 
     // state
     private Vector2 _paddleToBallVector;
@@ -14,44 +15,66 @@ public class Ball : MonoBehaviour
 
     //cached component references
     private AudioSource _myAudioSource;
+    private Rigidbody2D _myRigidbody2D;
 
     // Use this for initialization
 
-    void Start()
+    private void Start()
     {
         _paddleToBallVector = transform.position - paddle1.transform.position;
         _myAudioSource = GetComponent<AudioSource>();
+        _myRigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (_hasStarted) return;
-        LockBallToPaddle();
-        LaunchBallOnMouseClick();
+        if (!_hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchBallOnMouseClick();
+        }
     }
 
     private void LaunchBallOnMouseClick()
     {
+
         if (Input.GetMouseButtonDown(0))
         {
             _hasStarted = true;
-            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+           _myRigidbody2D.velocity = new Vector2(xPush, yPush);
         }
     }
 
     private void LockBallToPaddle()
     {
-        Vector2 paddlePosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        var paddlePosition = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePosition + _paddleToBallVector;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+       
         if (_hasStarted)
         {
-            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
-            _myAudioSource.PlayOneShot(clip);
+            PlayBallSounds();
+            RicochetBall();
         }
     }
+
+    private void PlayBallSounds()
+    {
+        AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+        _myAudioSource.PlayOneShot(clip);
+    }
+
+    private void RicochetBall()
+    {
+        var velocityTweak = new Vector2
+        (Random.Range(0f, randomFactor),
+            Random.Range(0f, randomFactor));
+        _myRigidbody2D.velocity += velocityTweak;
+    }
+
+
 }
