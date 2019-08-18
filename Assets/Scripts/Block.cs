c112d5a8ab6c6fa154f0b31a2081ce1649bf2103
mjsauro <mjsauro@gmail.com>
2019-08-17 23:36:59 -0500

    added sparkles vfx

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,6 +6,7 @@ public class Block : MonoBehaviour
 {
 
     [SerializeField] private AudioClip breakSound;
+    [SerializeField] private GameObject blockSparkleVFX;
 
     //cached reference
     private Level _level;
@@ -23,10 +24,22 @@ public class Block : MonoBehaviour
 
     private void DestroyBlock()
     {
-        FindObjectOfType<GameSession>().AddToScore();
-        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        PlayBlockDestroySFX();
         Destroy(this.gameObject);
         _level.BlockDestroyed();
+        TriggerSparklesVFX();
 
+    }
+
+    private void PlayBlockDestroySFX()
+    {
+        FindObjectOfType<GameSession>().AddToScore();
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+    }
+
+    private void TriggerSparklesVFX()
+    {
+        GameObject sparkles = Instantiate(blockSparkleVFX, transform.position, transform.rotation);
+        Destroy(sparkles, 1f);
     }
 }
